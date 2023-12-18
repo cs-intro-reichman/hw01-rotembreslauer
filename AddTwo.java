0417033165b3f4ad13355b3c454424e6f4cc3e93
rotembreslauer <154240191+rotembreslauer@users.noreply.github.com>
2023-12-18 19:38:00 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,9 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+	int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+    System.out.println( a + " + " + b + " = " + (a + b));
 	}
 }
