e7223804319f7a8a837c259416e34be04476fd6c
rotembreslauer <rotem.breslauer@post.runi.ac.il>
2023-12-20 15:01:21 +0200

    third

--- Triangle.java
+++ Triangle.java
@@ -3,8 +3,15 @@
  * This is known as the Triangle Inequality Theorem. 
  * Write a program that tests if three given integers form a triangle.
  */ 
-public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
+		public class Triangle{
+    public static void main(String[] args) {
+        Integer a = Integer.parseInt(args[0]);
+        Integer b = Integer.parseInt(args[1]);
+        Integer c = Integer.parseInt(args[2]);
+
+        boolean i = ((a + b) > c && (a + c) > b && (b + c) > a);
+           System.out.println(a + " , " + b + " , " + c +": " + i);
+     
+    
 	}
 }
